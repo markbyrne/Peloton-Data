e95764184a91b4915df32959d4f84a75ca472aca
Mark P Byrne <3760653+markbyrne@users.noreply.github.com>
2022-09-23 08:07:59 -0500

    2.0 - added all archived workout data

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ This dataset is a compilation of the Peloton data ecosystem.
 
 ### Versioning
 1.0 - All public-facing data
+2.0 - All Workout Data
+
+### Usage
+Main datasets are instructors.csv and workouts.csv. Any '_id' in instructors or workouts data references data in the metadata folder.
+
+    workouts.csv
+    class_type_ids: ['bf6639f2e50a4f0395cb1479542ed4bd']
+    
+    metadata/class_types-metadata.csv
+    bf6639f2e50a4f0395cb1479542ed4bd,Climb,Climb,cycling,True,107
+    
 
 ### Sources
 https://github.com/geudrik/peloton-client-library/blob/master/API_DOCS.md
--- peloton-scraper/__init__.py
+++ peloton-scraper/__init__.py
@@ -9,16 +9,20 @@ __date__ = "20220920"
 
 import requests
 import pandas as pd
+import os
 
 
 api_url = "https://api.onepeloton.com"
 
 
-def get_data(endpoint, payload=None):
+def get_data(endpoint, payload=None, session=None):
+	if session is None:
+		session = requests.Session()
+
 	print(f"Getting {endpoint} data...")
 	end_point = f"/api/{endpoint}"
 
-	response = requests.get(f"{api_url}{end_point}", params=payload)
+	response = session.get(f"{api_url}{end_point}", params=payload)
 	print(response)
 	if response.status_code < 200 or response.status_code >= 300:
 		raise Exception(f"Bad response: {response.status_code}")
@@ -32,27 +36,43 @@ def get_data(endpoint, payload=None):
 		total = None
 
 	try:
-		data = json["data"]
+		if len(json["data"]) > 0:
+			data = json["data"]
+			print("DATA FLAG FOUND")
+		else:
+			data = json
+			print("DATA FLAG NOT FOUND - Handle data in calling function")
 	except KeyError:
 		data = json
 
 	try:
+		limit = json['limit']
+	except KeyError:
+		try:
+			limit = json['count']
+		except KeyError:
+			limit = 20
+
+	try:
 		pages = json["page_count"]
+		print(f"ADDITIONAL {pages} PAGES FOUND")
 
 		for page in range(1, pages):
-			payload = {"page": page, "limit":  json["limit"]}
-			response = requests.get(f"{api_url}{end_point}", params=payload)
+			print(f"Getting Data from page {page}")
+			payload = {"page": page, "limit":  limit}
+			response = session.get(f"{api_url}{end_point}", params=payload)
 			json = response.json()
 
 			data.extend(json["data"])
-
+		print(f"Got Data from {pages} pages.")
 	except KeyError:
+		print("PAGE ITERATION UNSUCCESSFUL")
 		pass
 
 	if total:
 		print(f"Total: {total} | Captured: {len(data)}")
 		if total != len(data):
-			raise Exception("Not all data seems to have been captured")
+			raise Warning("Not all data seems to have been captured")
 	else:
 		print(f"Captured: {len(data)}")
 
@@ -80,12 +100,35 @@ def get_metadata():
 		try:
 			df.set_index("id", inplace=True)
 			print(df.head(10))
-			df.to_csv(path_or_buf=f"../datasets/{key}-metadata.csv")
+			df.to_csv(path_or_buf=f"../datasets/metadata/{key}-metadata.csv")
 		except KeyError:
 			print(df.head(10))
-			df.to_csv(path_or_buf=f"../datasets/{key}-metadata.csv", index=False)
+			df.to_csv(path_or_buf=f"../datasets/metadata/{key}-metadata.csv", index=False)
+
+
+def get_workouts(s):
+	# {"browse_category": "Cycling"}
+	data = get_data("v2/ride/archived", payload={"limit": "100"}, session=s)
+
+	print(data)
+
+	df = pd.DataFrame(data)
+	try:
+		df.set_index("id", inplace=True)
+		print(df.head(10))
+		df.to_csv(path_or_buf=f"../datasets/workouts.csv")
+	except KeyError:
+		print(df.head(10))
+		df.to_csv(path_or_buf=f"../datasets/workouts.csv", index=False)
 
 
 if __name__ == "__main__":
-	# get_instructors()
+	get_instructors()
 	get_metadata()
+
+	s = requests.Session()
+	payload = {'username_or_email': os.environ.get('EMAIL'), 'password': os.environ.get('PASS')}
+	s.post('https://api.onepeloton.com/auth/login', json=payload)
+	print(s.headers)
+
+	get_workouts(s)
